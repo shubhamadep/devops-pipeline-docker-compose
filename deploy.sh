742f7ffd9e71e386c0f72f614ee83fb05424eacc
Shubham <shubham.adep@wsu.edu>
2020-05-31 11:02:45 -0700

    removed cd from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "/VSTSAgent/_work/r1/a/_devops-deploy2 - CI/drop/"
+#cd "/VSTSAgent/_work/r1/a/_devops-deploy2 - CI/drop/"
 sudo docker-compose stop
 sudo docker-compose rm -f
 docker-compose up -d
